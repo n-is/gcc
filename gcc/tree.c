c90ed45b992383e27328b3f9b9ef268aef32cde7
Richard Sandiford <richard.sandiford@linaro.org>
2017-03-30 11:37:49 +0000

    Replace FMA_EXPR with one internal fn per optab
    There are four optabs for various forms of fused multiply-add:
    fma, fms, fnma and fnms.  Of these, only fma had a direct gimple
    representation.  For the other three we relied on special pattern-
    matching during expand, although tree-ssa-math-opts.c did have
    some code to try to second-guess what expand would do.
    
    This patch removes the old FMA_EXPR representation of fma and
    introduces four new internal functions, one for each optab.
    IFN_FMA is tied to BUILT_IN_FMA* while the other three are
    independent directly-mapped internal functions.  It's then
    possible to do the pattern-matching in match.pd and
    tree-ssa-math-opts.c (via folding) can select the exact
    FMA-based operation.
    
    The patch removes the gimple FE support for __FMA rather than mapping
    it to the internal function.  There's no reason now to treat it
    differently from other internal functions (although the FE doesn't
    handle those yet).
    
    The BRIG & HSA parts are a best guess, but seem relatively simple.

--- gcc/tree.c
+++ gcc/tree.c
@@ -7282,7 +7282,6 @@ commutative_ternary_tree_code (enum tree_code code)
     case WIDEN_MULT_PLUS_EXPR:
     case WIDEN_MULT_MINUS_EXPR:
     case DOT_PROD_EXPR:
-    case FMA_EXPR:
       return true;
 
     default:
@@ -7573,7 +7572,6 @@ add_expr (const_tree t, inchash::hash &hstate, unsigned int flags)
 	      flags &= ~OEP_ADDRESS_OF;
 	      break;
 
-	    case FMA_EXPR:
 	    case WIDEN_MULT_PLUS_EXPR:
 	    case WIDEN_MULT_MINUS_EXPR:
 	      {
